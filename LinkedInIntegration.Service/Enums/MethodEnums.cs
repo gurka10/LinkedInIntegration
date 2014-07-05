701a04c0395385090e8b1f2634aaa72429726fbe
gurka10 <gurka10@hotmail.com>
2014-07-05 23:38:47 +0100

    Refactored Enum

---
+++ LinkedInIntegration.Service/Enums/MethodEnums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedInIntegration.Service.Enums
+{
+    public class MethodEnums
+    {
+        public enum Method { GET, POST, PUT, DELETE };
+    }
+}
